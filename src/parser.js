b07d5a1b83b7a8222370fdad5e9420947138d1d9
Tolokoban <contact@tolokoban.org>
2015-07-02 17:12:03 +0200

    Work begins\!

--- src/parser.js
+++ src/parser.js
@@ -6,20 +6,30 @@ var StateMachine = require("tlk-state-machine");
 function pushText() {
   var cursor = this.vars.cursor || 0;
   var text = this.source.substr(cursor, this.index - cursor);
-  console.log('"" + text + '"');
+  this.vars.nodes.push(
+    {
+      node: Tree.TEXT,
+      text: text
+    }
+  );
+}
+
+function setCursor() {
+  this.vars.cursor = this.index + 1;
 }
 
 var rules = {
   TEXT: [
     ["<", "OPEN_TAG", pushText]
+  ],
+  OPEN_TAG: [
+    [">", "TEXT", setCursor]
   ]
 };
 
 
-
 module.exports = function(source) {
-  return {
-    node: Tree.TEXT,
-    text: source
-  };
+  var ctx = StateMachine.run(rules, source, {cursor: 0, nodes: []});
+  if (ctx.state == "TEXT") pushText.call(ctx);
+  return ctx.vars;
 };
