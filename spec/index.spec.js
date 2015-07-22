e83bde53608c263e08465c9eebb51fbea6320201
Tolokoban <fabien.petitjean@free.fr>
2015-07-22 20:34:43 +0200

    First tests.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,7 +3,24 @@
 var Tree = require("../index");
 
 describe("Parsing", function() {
-  it("should parse simple text", function() {
-    expect(Tree.parseString("Hello World!")).toEqual({node: Tree.TEXT, text: "Hello World!"});
-  });
-});
+           it("should parse simple text", function() {
+                var result = Tree.parseString("Hello World!");
+                expect(result).toEqual([{node: Tree.TEXT, text: "Hello World!"}]);
+              });
+           it("should parse bold tag", function() {
+                var result = Tree.parseString("Hello <b>World</b>!");
+                expect(result).toEqual(
+                  [
+                    {node: Tree.TEXT, text: "Hello "},
+                    {
+                      node: Tree.TAG,
+                      name: "b",
+                      children: [
+                        {node: Tree.TEXT, text: "World"}
+                      ]
+                    },
+                    {node: Tree.TEXT, text: "!"}
+                  ]
+                );
+              });
+         });
--- src/parser.js
+++ src/parser.js
@@ -14,8 +14,30 @@ function pushText() {
   );
 }
 
+function tagName() {
+  if (this.vars.tag && this.vars.tag.name) return;
+  this.vars.tag = {name: this.source.substr(cursor, this.index - cursor).trim()};
+}
+
 function setCursor() {
   this.vars.cursor = this.index + 1;
+}
+
+function all() {
+  var args = [];
+  var i, arg;
+  for (i = 0 ; i < arguments.length ; i++) {
+    arg = arguments[i];
+    args.push(arg);
+  }
+  return function() {
+   args.forEach(
+     function(f) {
+       f.call(this);
+     },
+     this
+   );
+  };
 }
 
 var rules = {
@@ -23,13 +45,15 @@ var rules = {
     ["<", "OPEN_TAG", pushText]
   ],
   OPEN_TAG: [
+    ["/"],
+    [" ", "ATTRIBS", tagName],
     [">", "TEXT", setCursor]
   ]
 };
 
 
 module.exports = function(source) {
-  var ctx = StateMachine.run(rules, source, {cursor: 0, nodes: []});
+  var ctx = StateMachine.run(rules, source, {cursor: 0, children: []});
   if (ctx.state == "TEXT") pushText.call(ctx);
-  return ctx.vars;
+  return ctx.vars.children;
 };
